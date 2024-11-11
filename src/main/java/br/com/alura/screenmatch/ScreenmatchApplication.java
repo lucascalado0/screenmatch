31e1f231135f22d05795ce73f5c8da3255df468a
devlucascalado <devlucascalado@gmail.com>
2024-11-11 19:04:04 -0300

    novas classes

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,9 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +19,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
 
-		var json =  consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Seanson=1&apikey=7d610144");
+		var json =  consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=7d610144");
 
 		System.out.println(json);
 
 		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		//System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+		System.out.println(dados);
+
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=1&apikey=7d610144");
+
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+
+		System.out.println(dadosEpisodio);
+
 	}
 }
