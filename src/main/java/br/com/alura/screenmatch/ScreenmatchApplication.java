225be7dfed58b2bc4641b4f33db21cbb7a2d6072
devlucascalado <devlucascalado@gmail.com>
2024-11-11 11:55:09 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+
+		var json =  consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Seanson=1&apikey=7d610144");
+
+		System.out.println(json);
+
+		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+
+		System.out.println(json);
+	}
+}
