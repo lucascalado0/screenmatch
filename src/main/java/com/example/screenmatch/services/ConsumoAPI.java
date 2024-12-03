82c96953fcf74a4c3c92dcfc9b1e0a2b2cba7c89
devlucascalado <devlucascalado@gmail.com>
2024-12-03 16:50:13 -0300

    requisicao api

--- src/main/java/com/example/screenmatch/services/ConsumoAPI.java
+++ src/main/java/com/example/screenmatch/services/ConsumoAPI.java
@@ -1,5 +1,30 @@
 package com.example.screenmatch.services;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.rmi.RemoteException;
+
 public class ConsumoAPI {
-    Htt
+
+    public String obterDados(String endereco) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RemoteException();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String json = response.body();
+        return json;
+    }
+
 }
